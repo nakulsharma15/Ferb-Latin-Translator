298aa7932932034671d8465e38b17c2fd5953a05
nakulsharma15 <nakuls309@gmail.com>
2021-08-29 17:32:29 +0530

    Ferb Latin Converter

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+function getTranslationURL(text) {
+   return serverURL + "?text=" + text;
+}
+
+function errorHandler(error) {
+   alert("Error Occurred, Please try after some time.")
+}
+
+function clickEventHandler(){
+  
+  var inputTxt = txtInput.value;
+
+  fetch(getTranslationURL(inputTxt))
+  .then(response => response.json())
+  .then(json => {
+     var translatedText = json.contents.translated;
+     outputDiv.innerText = translatedText;
+  })
+
+  .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click",clickEventHandler);
